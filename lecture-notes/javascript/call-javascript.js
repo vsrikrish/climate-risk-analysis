0da47889e7cbaa474e72ebbf0f720df0d3f842f4
vsrikrish <vs498@cornell.edu>
2023-01-02 15:21:20 +0000

    Deploying to gh-pages from  @ a6e7fe83cc97ac9cc1647df4532e674dabc99326 🚀

---
+++ lecture-notes/javascript/call-javascript.js
@@ -0,0 +1,39 @@
+// Idea taken from http://docs.mathjax.org/en/latest/web/configuration.html and https://github.com/jonathanlilly/liminal
+
+//Call remark.js 
+
+remark.macros.img = function (altText, width) {
+    var url = this;
+    return '<img alt="' + altText + '" src="' + url + '" style="width: ' + width + '" />';
+};
+
+remark.macros.link = function (text) {
+    var url = this;
+    return '<a href="' + url  + '" target="_blank"/>' + text + '</a>';
+};
+
+var slideshow = remark.create({
+    ratio: '16:9',
+    navigation: {
+        click: false,
+        scroll: false
+    },
+    properties: {
+        class: "center, middle"
+    },
+    countIncrementalSlides: false,
+    highlightLanguage: 'julia',
+    highlightStyle: 'googlecode'
+});
+
+//Call KaTeX with auto-rendering, see https://katex.org/docs/autorender.html
+document.addEventListener("DOMContentLoaded", function () {
+    renderMathInElement(document.body, {
+        delimiters: [
+            {left: "$$",right: "$$",display: true},
+            {left: "$",right: "$",display: false},
+            {left: "\\[",right: "\\]", display: true},
+            {left: "\\(",right: "\\)",display: false}
+                    ]
+    });
+});
